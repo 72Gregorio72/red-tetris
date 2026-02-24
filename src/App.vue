0ccc1bd4a4239224d9c43e47af5725693cd8a52e
Francescotersi <francescotersillo@gmail.com>
2026-02-24 14:21:58 +0100

    daren the strongest man alive

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
+import BackGroundView from './views/BackGroundView.vue';
+
 </script>
 
 <!-- in questo modo App.vue viene usato per fare il display di solo quello che dice il path del router -->
  <!-- se si vuole aggiungere qualcosa che viene sempre visualizzato va messo qua -->
 <template>
-  <router-view />
+  <back-ground-view>
+    <router-view />
+  </back-ground-view>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
---
+++ src/Style/Buttons.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+function startGame(path: string) {
+	router.push(path);
+}
+
+</script>
+
+<template>
+	<div class="button-container">
+		<button class="play-button" @click="startGame('/game')">
+			<img src="../../public/asset/play_button.png" alt="gioca" class="button-image"/>
+		</button>
+	</div>
+</template>
+
+<style scoped>
+
+.button-container {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	transform: translateY(0px);
+}
+
+.play-button {
+	display:flex;
+	background: none;
+	border: none;
+	cursor: pointer;
+	width: 250px;
+}
+
+.button-image {
+	display: flex;
+	width: 100%;
+}
+
+.play-button:hover {
+    transform: scale(1.05);
+}
+
+.play-button:active {
+    transform: scale(0.95);
+}
+
+
+</style>
